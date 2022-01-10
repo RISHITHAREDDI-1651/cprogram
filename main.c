2b440d661683bac72a9b518e33122428b78701c1
REDDI-RISHITHAR <97424663+RISHITHAREDDI-1651@users.noreply.github.com>
2022-01-10 05:16:12 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void max of two numbers;
+int main ();
+{
+  max of two numbers;
+}
+void max of two numbers()
+{
+  int num1=20,num2=30;
+  if (num1>num2);
+  {
+  printf ("num1 is greater than num2");
+  }
+  else 
+  {
+    printf ("num2 is greater than num1");
+  }
+}
